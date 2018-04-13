0d11e09f06f0040edf9d54dbc4c196185de99159
nicksoule <nicksoule@live.com>
2018-04-13 16:50:27 -0400

    fixed API connection problem and cleaned up project a little

--- src/com/gc/controller/HomeController.java
+++ src/com/gc/controller/HomeController.java
@@ -17,8 +17,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-
-import com.gc.model.Person;
 import com.gc.model.Record;
 import com.gc.util.APIBuild;
 import com.gc.util.APICredentials;
@@ -61,25 +59,10 @@ public class HomeController {
 					idArray.add(jsonID.getJSONArray("findCompletedItemsResponse").getJSONObject(0)
 							.getJSONArray("searchResult").getJSONObject(0).getJSONArray("item").getJSONObject(k)
 							.getJSONArray("itemId").get(0).toString());
-					
-//					for (int m = 0; m <= 0; m++) {
-//						JSONObject jRec = APIBuild.getRecordInfo(idArray.get(m));
-//						System.out.println(jRec.toString());
-//						Record rec = new Record();
-//						rec.setTitle(jRec.getJSONObject("Item").get("Title").toString());
-//						rec.setBody(jRec.getJSONObject("Item").get("Description").toString());
-//						rec.setDate(jRec.getJSONObject("Item").get("EndTime").toString());
-//
-//						rec.setPrice(jRec.getJSONObject("Item").getJSONObject("ConvertedCurrentPrice").get("Value").toString());
-//						rec.setImage(jRec.getJSONObject("Item").getJSONArray("PictureURL").get(0).toString());
-//						
-//						recArray.add(rec);
-//					}
 				}
 			}
 
 			System.out.println(idArray);
-//			System.out.println(idArray.get(0));
 			
 			for (int m = 0; m <= 3; m++) {
 				System.out.println(idArray.get(m));
@@ -109,31 +92,4 @@ public class HomeController {
 		return "index";
 	}
 
-	@RequestMapping(value = "success", method = RequestMethod.POST)
-	// this method call will process the data from the form and push it back to the
-	// success.jsp
-	public ModelAndView registerSuccess(@RequestParam("fName") String firstName, @RequestParam("lName") String lastName,
-			@RequestParam("gender") String gender, @RequestParam("phone") String phone,
-			@RequestParam("favorites") String checkbox, Model model) { // added model to be able to pass my arraylist in
-																		// later
-
-		Person p = new Person(firstName, lastName, phone, gender, checkbox.split(","));
-
-		String sayHello = "Hello, " + p.getFirstName() + " " + p.getGender() + " " + p.getFavorites()[0];
-
-		// Example showing ArrayList push to front end using JSTL to loop through
-		ArrayList<Person> personList = new ArrayList<Person>();
-		personList.add(new Person("Antonella", "Solomon", "313.555.1212", "female", "Decaf,Mocha".split(",")));
-		personList.add(new Person("Cara", "Test", "313.555.3312", "female", "Decaf,Chocolate".split(",")));
-		personList.add(new Person("James", "test2", "313.553.1212", "male", "Marshmallows,Mocha".split(",")));
-		personList.add(new Person("Ron", "Test3", "313.545.1212", "male", "Decaf,Mocha".split(",")));
-		personList.add(new Person("Zach", "Test4", "313.655.1215", "female", "Decaf,Mocha".split(",")));
-		personList.add(new Person("Ben", "Test5", "313.555.1212", "male", "Decaf,Regular".split(",")));
-		model.addAttribute("pList", personList); // this is used to pass the arraylist into the jsp page
-
-		// first parameter : the name of the jsp
-		// second parameter : name of the EL tag variable name
-		// third parameter : object or data that should go back into our jsp page
-		return new ModelAndView("success", "helloMsg", sayHello);
-	}
 }
